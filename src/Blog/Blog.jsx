6aad0abecfd420e7a17ae8572b50c08bb43a40a8
Cristhianmca <cristhianmc84@gmail.com>
2024-05-15 23:02:06 -0500

    arreglando nuestras carreras

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -32,7 +32,7 @@ const Blog = () =>{
             <div className='card_blog'>
                 <img className='card__imagen' src="https://i.postimg.cc/YCYDkZQq/charlaconta.jpg" alt="" />
                 <h5 className='card__titulo'>Titulo</h5>
-                <p className='card__paragraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui natus obcaecati earum, sit dolorem suscipit officia aliquam magnam totam eos!</p>
+                <p className='card__paragraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui natus obcaecati earum</p>
                 <p>Ver mas </p>
             </div>
         </div>
