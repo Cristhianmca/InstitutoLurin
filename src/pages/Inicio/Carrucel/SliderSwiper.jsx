fb06316bab9e4e97d7fce619429676c564c8336b
cesarmarca <cesar.14.2012@gmail.com>
2024-07-10 01:18:18 -0500

    cesargg

--- src/pages/Inicio/Carrucel/SliderSwiper.jsx
+++ src/pages/Inicio/Carrucel/SliderSwiper.jsx
@@ -12,19 +12,22 @@ const MyCarousel = () => (
       showStatus={false} 
       autoPlay={true} 
       interval={3000}
-      showIndicators ={true}
+      showIndicators ={false}/*linea que cubre todo*/
     
     >
       <div>
-        <img src="https://res.cloudinary.com/dxhcv6buy/image/upload/v1720292271/qghz54utluk14uiitvj4.jpg" alt="Slide 1" />
+        <img src="https://res.cloudinary.com/dqwksoy7s/image/upload/v1720591673/mercadoshop_venta_de_productos_promociones_tecnologia_negro_fmusbp.png
+
+
+" />
         
       </div>
       <div>
-        <img src="https://res.cloudinary.com/dxhcv6buy/image/upload/v1720292271/qghz54utluk14uiitvj4.jpg" alt="Slide 2" />
+        <img src="https://res.cloudinary.com/dqwksoy7s/image/upload/v1720591673/mercadoshop_venta_de_productos_promociones_tecnologia_negro_fmusbp.png" />
         
       </div>
       <div>
-        <img src="https://res.cloudinary.com/dxhcv6buy/image/upload/v1720292271/qghz54utluk14uiitvj4.jpg" alt="Slide 3" />
+        <img src="https://res.cloudinary.com/dqwksoy7s/image/upload/v1720591673/mercadoshop_venta_de_productos_promociones_tecnologia_negro_fmusbp.png" alt="Slide 3" />
         
       </div>
       <div>
