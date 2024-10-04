43758828d4e5e85e19f3670ba8108a7a6a436b30
Cristhianmca <cristhianmc84@gmail.com>
2024-10-04 18:14:54 -0500

    arreglando el menu nav

--- src/pages/Inicio/Header/Nav.jsx
+++ src/pages/Inicio/Header/Nav.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 
-function Nav() {
+export default function Nav() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [activeSubmenu, setActiveSubmenu] = useState(null);
 
@@ -17,57 +17,53 @@ function Nav() {
 
   return (
     <nav className="container_nav">
-      <div className="menu-icon" onClick={toggleMenu}>
-        <svg className='svg_icon' viewBox="0 0 150 80" width="40" height="40">
-          <rect width="80" height="15"></rect>
-          <rect y="30" width="80" height="15"></rect>
-          <rect y="60" width="80" height="15"></rect>
-        </svg>
+      <div className="nav-content">
+        <Link to="/" className="logo-link">
+          <img className="logo_instituto" src="/lurin.png" alt="Logo Instituto" />
+        </Link>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <svg className='svg_icon' viewBox="0 0 100 80" width="30" height="30">
+            <rect width="100" height="15"></rect>
+            <rect y="30" width="100" height="15"></rect>
+            <rect y="60" width="100" height="15"></rect>
+          </svg>
+        </div>
+        <ul className={`cont-ul ${menuOpen ? 'active' : ''}`}>
+          <li className="develop"><Link to="/" className="link" onClick={toggleMenu}>Inicio</Link></li>
+          <li className="develop"><Link to="/nosotros" className="link" onClick={toggleMenu}>Nosotros</Link></li>
+          <li className="develop" onClick={() => toggleSubmenu(0)}>
+            Carreras
+            <ul className={`ul-second ${activeSubmenu === 0 ? 'active' : ''}`}>
+              <li className="develop"><Link to="/diseno" className="link" onClick={toggleMenu}>Diseño y Desarrollo Web</Link></li>
+              <li className="develop"><Link to="/protesis" className="link" onClick={toggleMenu}>Prótesis</Link></li>
+              <li className="develop"><Link to="/contabilidad" className="link" onClick={toggleMenu}>Contabilidad</Link></li>
+            </ul>
+          </li>
+          <li className="develop" onClick={() => toggleSubmenu(1)}>
+            Admisión
+            <ul className={`ul-second ${activeSubmenu === 1 ? 'active' : ''}`}>
+              <li className="develop"><Link to="/examen" className="link" onClick={toggleMenu}>Examen 2024</Link></li>
+            </ul>
+          </li>
+          <li className="develop" onClick={() => toggleSubmenu(2)}>
+            Noticias
+            <ul className={`ul-second ${activeSubmenu === 2 ? 'active' : ''}`}>
+              <li className="develop"><Link to="/blog" className="link" onClick={toggleMenu}>Blog</Link></li>
+            </ul>
+          </li>
+          <li className="develop">
+            <Link to="/contactanos" className="link" onClick={toggleMenu}>Contáctanos <i className="fa-brands fa-whatsapp"></i></Link>
+          </li>
+          <li className="develop">
+            <a className="biblioteca" href="https://centroderecursosies.drelm.gob.pe/" target="_blank" rel="noopener noreferrer" onClick={toggleMenu}>
+              Biblioteca Virtual <i className="fa-sharp fa-solid fa-books"></i>
+            </a>
+          </li>
+          <li className="develop">
+            <Link to="/aula-virtual" className="link" onClick={toggleMenu}>Aula Virtual</Link>
+          </li>
+        </ul>
       </div>
-      <Link to="/">
-        <img className="logo_instituto" src="https://res.cloudinary.com/dxhcv6buy/image/upload/v1717457856/images_pcrpfl.png" alt="" />
-      </Link>
-      <ul className={`cont-ul ${menuOpen ? 'active' : ''}`}>
-        <li className="develop"><Link to="/" className="link">Inicio</Link></li>
-        <li className="develop"><Link to="/nosotros" className="link">Nosotros</Link></li>
-        <li className="develop" onClick={() => toggleSubmenu(0)}>
-          Carreras
-          <ul className={`ul-second ${activeSubmenu === 0 ? 'active' : ''}`}>
-            <li className="develop"><Link to="/diseño" className="link">Diseño y Desarrollo Web</Link></li>
-            <li className="develop"><Link to="/protesis" className="link">Protesis</Link></li>
-            <li className="develop"><Link to="/contabilidad" className="link">Contabilidad</Link></li>
-          </ul>
-        </li>
-        <li className="develop" onClick={() => toggleSubmenu(1)}>
-          Admisión
-          <ul className={`ul-second ${activeSubmenu === 1 ? 'active' : ''}`}>
-            <li className="develop"><Link to="/examen" className="link">Exámen 2024</Link></li>
-          </ul>
-        </li>
-        <li className="develop" onClick={() => toggleSubmenu(2)}>
-          Noticias
-          <ul className={`ul-second ${activeSubmenu === 2 ? 'active' : ''}`}>
-            <li className="develop"><Link to="/blog" className="link">Blog</Link></li>
-          </ul>
-        </li>
-        <li className="develop">
-          <Link to="/contactanos" className="link">
-            Contáctanos <i className="fa-brands fa-whatsapp"></i>
-          </Link>
-        </li>
-        <li className="develop">
-          <a className="biblioteca" href="https://centroderecursosies.drelm.gob.pe/" target="_blank" rel="noopener noreferrer">
-            Biblioteca Virtual <i className="fa-sharp fa-solid fa-books"></i>
-          </a>
-        </li>
-        <li className="develop">
-          <Link to="/aula-virtual" className="link">
-            Aula Virtual
-          </Link>
-        </li>
-      </ul>
     </nav>
   );
 }
-
-export default Nav;
